ea2512f8e9c6781594a0d856db64bb17cdd63d9e
luis <lrlitang18@gmail.com>
2023-04-28 17:43:56 -0500

    feat: Add navbar component

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import Navbar from "@/shared/components/navbar.vue";
+</script>
+
+<template>
+  <Navbar/>
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+
+}
+</style>
---
+++ src/shared/components/navbar.vue
@@ -0,0 +1,57 @@
+<template>
+
+  <nav class="navbar" @scroll>
+      <ul>
+          <li class="navbar-link">
+              <router-link to="/home">Home</router-link>
+          </li>
+          <li class="navbar-link">
+              <router-link to="/home" >Remodelers</router-link>
+          </li>
+          <li class="navbar-link">
+              <router-link to="/home" >Create Post</router-link>
+          </li>
+      </ul>
+  </nav>
+    <div class="introduction">
+        <div>background</div>
+    </div>
+    <router-view></router-view>
+
+
+</template>
+
+<script>
+export default {
+    name: "navbar"
+}
+</script>
+
+<style scoped>
+body, html {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Poppins;
+}
+  .navbar {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #004A63;
+  }
+  .navbar-link{
+      list-style-type: none;
+      display: inline-block;
+      padding: 10px 20px;
+  }
+  .navbar-link a{
+      color: white;
+      text-decoration: none;
+      font-weight: bold;
+  }
+  .introduction{
+      background-color: #004A63;
+      height: 500px;
+  }
+</style>
